0d28202c4ddb7ad1c4411ba00009015b9484226e
mvasylchyshyn <mykola.vasylchyshyn@globallogic.com>
2014-01-14 20:00:56 +0200

    started chapter 28

---
+++ lua52/ch28/ch28.c
@@ -0,0 +1,28 @@
+#include <lua.h>
+#include <lauxlib.h>
+
+static int l_map(lua_State *L) {
+	int i, n;
+	luaL_checktype(L, 1, LUA_TTABLE);
+	luaL_checktype(L, 2, LUA_TFUNCTION);
+
+	n = luaL_len(L, 1);
+	for (i = 1; i <= n; i++) {
+		lua_pushvalue(L, 2);
+		lua_rawgeti(L, 1, i);
+		lua_call(L, 1, 1);
+		lua_rawseti(L, 1, i);
+	}
+
+	return 0;
+}
+
+static struct luaL_Reg ch28_functions[] = {
+	{ "map", l_map },
+	{ NULL, NULL }
+};
+
+int luaopen_ch28(lua_State *L) {
+	luaL_newlib(L, ch28_functions);
+	return 1;
+}
---
+++ lua52/ch28/test.lua
@@ -0,0 +1,10 @@
+#!/usr/bin/env lua
+
+lib = require("ch28")
+
+t = { 10, 20, 30, 40 }
+lib.map(t, function(arg) return arg/10 end)
+
+for _,v in ipairs(t) do
+	print(v)
+end
